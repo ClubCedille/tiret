620dc3fde5d85529db55a703fd80e322fb17250d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-14 13:01:40 -0500

    Class Repository

---
+++ repository.py
@@ -0,0 +1,46 @@
+class Repository:
+	"""
+	This class gathers data about one GitHub repository.
+	"""
+
+	def __init__(self, name, description, stars, languages):
+		"""
+		The constructor requires all the data that the class is meant to
+		contain.
+
+		Args:
+			name (str): the repository's name
+			description (str): the repository's description
+			stars (int): the repository's number of stars
+			languages: a list, set or tuple of the computer languages (str)
+				used in the repository
+		"""
+		self._name = name
+		self._description = description
+		self._stars = stars
+
+		if isinstance(languages, tuple):
+			self._languages = languages
+		else:
+			self._languages = tuple(languages)
+
+	def __repr__(self):
+		return self.__class__.__name__\
+			+ f"('{self._name}', '{self._description}', "\
+			+ f"{self._stars}, {self._languages})"
+
+	@property
+	def description(self):
+		return self._description
+
+	@property
+	def languages(self):
+		return self._languages
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def stars(self):
+		return self.stars
--- tiret.py
+++ tiret.py
@@ -4,6 +4,8 @@ import requests
 # Temporaire
 from sys import argv
 
+from repository import Repository
+
 
 _KEY_DESC =  "description"
 _KEY_LANG = "languages"
@@ -20,8 +22,14 @@ def fetch_repo_info(owner, repo, username, token):
 	repo_url = _PATH_REPOS + owner + _SLASH + repo
 	repo_response = requests.get(repo_url, auth=auth_couple)
 	repo_data = json.loads(repo_response.content)
-	repo = _repo_from_api_data(repo_data)
-	_fetch_repo_languages(repo_url, auth_couple)
+
+	name = repo_data.get(_KEY_NAME)
+	description = repo_data.get(_KEY_DESC)
+	stars = repo_data.get(_KEY_STARS)
+	languages = _fetch_repo_languages(repo_url, auth_couple)
+	repo = Repository(name, description, stars, languages)
+
+	return repo
 
 
 def _fetch_repo_languages(repo_url, auth_couple):
@@ -29,17 +37,8 @@ def _fetch_repo_languages(repo_url, auth_couple):
 	lang_response = requests.get(repo_lang_url, auth=auth_couple)
 	lang_data = json.loads(lang_response.content)
 	languages = tuple(lang_data.keys())
-	print(languages)
 	return languages
 
 
-def _repo_from_api_data(repo_data):
-	name = repo_data.get(_KEY_NAME)
-	description = repo_data.get(_KEY_DESC)
-	stars = repo_data.get(_KEY_STARS)
-	print(name)
-	print(description)
-	print(stars)
-
-
-fetch_repo_info("ClubCedille", "trema", argv[1], argv[2])
+repo = fetch_repo_info("ClubCedille", "trema", argv[1], argv[2])
+print(repo)
