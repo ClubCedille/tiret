13aca4e5eb299b37b2f41b5bbbd96a5f1047ccaf
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-15 16:12:38 -0500

    Library argparse in the demo

--- demo.py
+++ demo.py
@@ -1,9 +1,29 @@
+"""
+This demo shows how to use function fetch_repo_info to obtain information about
+a GitHub repository. The user must provide the username of the repository's
+owner, the repository's name, their own username and one of their personal
+access tokens (PATs).
+"""
+
+
+from argparse import ArgumentParser
 from sys import argv
 
 from tiret import fetch_repo_info
 
 
-repo = fetch_repo_info("ClubCedille", "trema", argv[1], argv[2])
+parser = ArgumentParser(description=__doc__)
+parser.add_argument("-o", "--owner",
+	help="The username of the repository's owner")
+parser.add_argument("-r", "--repository",
+	help="The repository's name")
+parser.add_argument("-u", "--username",
+	help="Any GitHub username")
+parser.add_argument("-t", "--token",
+	help="A PAT owned by user -u")
+args = parser.parse_args()
+
+repo = fetch_repo_info(args.owner, args.repository, args.username, args.token)
 
 print("Representation")
 print(repo)
