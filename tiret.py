3faf59042d34bdc29efdc4178e8b077813c687c6
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-15 21:17:24 -0500

    Revert "Authentication works without a username."
    This reverts commit ac574996e48b466b228b0c608263afcb1731c49b.
    The header allows the request even when the PAT is incorrect.

--- tiret.py
+++ tiret.py
@@ -5,7 +5,6 @@ from repo_keys import *
 from repository import Repository
 
 
-_KEY_AUTHORIZATION = "Authorization"
 _KEY_LOGIN = "login"
 _KEY_STARGAZERS = "stargazers_count"
 
@@ -14,15 +13,16 @@ _PATH_REPOS = "https://api.github.com/repos/"
 _SLASH = "/"
 
 
-def fetch_repo_info(owner, repo, token):
+def fetch_repo_info(owner, repo, username, token):
 	"""
 	This function obtains information about a GitHub repository from the GitHub
-	API. It requires a GitHub personal access token (PAT) to authenticate the
-	requests.
+	API. It requires a GitHub username and a personal access token (PAT) to
+	authenticate the requests.
 
 	Args:
 		owner (str): the name of the repository's owner
 		repo (str): the repository's name
+		username (str): the name of any GitHub user
 		token (str): the user's PAT
 
 	Returns:
@@ -31,52 +31,52 @@ def fetch_repo_info(owner, repo, token):
 
 	Raises:
 		RuntimeError: if a request to the GitHub API fails. Status code 401
-			can be due to an incorrect PAT.
+			can be due to an incorrect username or PAT
 	"""
-	headers = {_KEY_AUTHORIZATION: token}
+	auth_couple = (username, token)
 	repo_url = _PATH_REPOS + owner + _SLASH + repo
-	repo_response = requests.get(repo_url, headers=headers)
+	repo_response = requests.get(repo_url, auth=auth_couple)
 	_raise_request_exception(repo_response.status_code)
 
 	repo_data = json.loads(repo_response.content)
 	name = repo_data.get(KEY_NAME)
 	description = repo_data.get(KEY_DESC)
 	stars = repo_data.get(_KEY_STARGAZERS)
-	contributors = _fetch_repo_contributors(repo_url, headers)
-	commits = _fetch_repo_num_commits(repo_url, headers)
-	languages = _fetch_repo_languages(repo_url, headers)
+	contributors = _fetch_repo_contributors(repo_url, auth_couple)
+	commits = _fetch_repo_num_commits(repo_url, auth_couple)
+	languages = _fetch_repo_languages(repo_url, auth_couple)
 	repo = Repository(
 		name, description, stars, contributors, commits, languages)
 
 	return repo
 
 
-def _fetch_repo_contributors(repo_url, headers):
+def _fetch_repo_contributors(repo_url, auth_couple):
 	contributor_url = repo_url + _SLASH + KEY_CONTRIBUTORS
-	contributor_response = requests.get(contributor_url, headers=headers)
+	contributor_response = requests.get(contributor_url, auth=auth_couple)
 	_raise_request_exception(contributor_response.status_code)
 	contributor_data = json.loads(contributor_response.content)
 	contributors = *(c[_KEY_LOGIN] for c in contributor_data),
 	return contributors
 
 
-def _fetch_repo_languages(repo_url, headers):
+def _fetch_repo_languages(repo_url, auth_couple):
 	repo_lang_url = repo_url + _SLASH + KEY_LANG
-	lang_response = requests.get(repo_lang_url, headers=headers)
+	lang_response = requests.get(repo_lang_url, auth=auth_couple)
 	_raise_request_exception(lang_response.status_code)
 	lang_data = json.loads(lang_response.content)
 	languages = tuple(lang_data.keys())
 	return languages
 
 
-def _fetch_repo_num_commits(repo_url, headers):
+def _fetch_repo_num_commits(repo_url, auth_couple):
 	repo_commit_url = repo_url + _SLASH + KEY_COMMITS + _PARAM_PAGE
 	page_num = 1
 	num_commits = 0
 
 	while True:
 		commit_response = requests.get(
-			repo_commit_url+str(page_num), headers=headers)
+			repo_commit_url+str(page_num), auth=auth_couple)
 		_raise_request_exception(commit_response.status_code)
 		commit_data = json.loads(commit_response.content)
 
