86306b9ebd413861d1a0727d0a4bb71cff10e94d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-12-15 20:38:31 -0500

    Exception for request failure

--- tiret.py
+++ tiret.py
@@ -28,12 +28,17 @@ def fetch_repo_info(owner, repo, username, token):
 	Returns:
 		Repository: an object containing information about the specified
 			repository
+
+	Raises:
+		RuntimeError: if a request to the GitHub API fails. Status code 401
+			can be due to an incorrect username or PAT
 	"""
 	auth_couple = (username, token)
 	repo_url = _PATH_REPOS + owner + _SLASH + repo
 	repo_response = requests.get(repo_url, auth=auth_couple)
-	repo_data = json.loads(repo_response.content)
+	_raise_request_exception(repo_response.status_code)
 
+	repo_data = json.loads(repo_response.content)
 	name = repo_data.get(KEY_NAME)
 	description = repo_data.get(KEY_DESC)
 	stars = repo_data.get(_KEY_STARGAZERS)
@@ -49,6 +54,7 @@ def fetch_repo_info(owner, repo, username, token):
 def _fetch_repo_contributors(repo_url, auth_couple):
 	contributor_url = repo_url + _SLASH + KEY_CONTRIBUTORS
 	contributor_response = requests.get(contributor_url, auth=auth_couple)
+	_raise_request_exception(contributor_response.status_code)
 	contributor_data = json.loads(contributor_response.content)
 	contributors = *(c[_KEY_LOGIN] for c in contributor_data),
 	return contributors
@@ -57,6 +63,7 @@ def _fetch_repo_contributors(repo_url, auth_couple):
 def _fetch_repo_languages(repo_url, auth_couple):
 	repo_lang_url = repo_url + _SLASH + KEY_LANG
 	lang_response = requests.get(repo_lang_url, auth=auth_couple)
+	_raise_request_exception(lang_response.status_code)
 	lang_data = json.loads(lang_response.content)
 	languages = tuple(lang_data.keys())
 	return languages
@@ -70,6 +77,7 @@ def _fetch_repo_num_commits(repo_url, auth_couple):
 	while True:
 		commit_response = requests.get(
 			repo_commit_url+str(page_num), auth=auth_couple)
+		_raise_request_exception(commit_response.status_code)
 		commit_data = json.loads(commit_response.content)
 
 		num_commits_page = len(commit_data)
@@ -79,3 +87,9 @@ def _fetch_repo_num_commits(repo_url, auth_couple):
 		page_num += 1
 
 	return num_commits
+
+
+def _raise_request_exception(status_code):
+	if status_code != 200:
+		raise RuntimeError(
+			f"Request to the GitHub API fialed. Status: {status_code}.")
